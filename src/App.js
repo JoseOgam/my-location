effd1249885b4ff09ad1ce68f14a0bee334744a9
JoseOgam <jotienoochieng@gmail.com>
2020-01-14 07:28:17 +0300

    :green_apple: initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          this is a milestone step in my life
         </p>
         <a
           className="App-link"
