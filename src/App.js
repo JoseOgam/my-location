5e71bd456754c8fea4b904c770d8060a69c5307e
Joseph <joseph@HP-EliteBook-820-G3-c90a9ed2.localdomain>
2023-08-12 09:04:01 +0300

    packages installation

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const { jobs, loading, error } = useFetchJobs();
   return (
     <Container>
-      <h2>Opps under Development</h2>
+      <h2>Opps under Development!!!</h2>
       <h3>GITHUB JOBS!</h3>
       {loading && <h3>loading...</h3>}
       {error && <h3>error refresh the page</h3>}
