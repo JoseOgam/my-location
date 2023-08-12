6dc7a0f2f0d2abf7e5b55c8b2d432d246ae873a2
Joseph <joseph@HP-EliteBook-820-G3-c90a9ed2.localdomain>
2023-08-12 06:56:12 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,25 @@
-import React, {Component} from "react";
-import {
-    withScriptjs,
-    withGoogleMap,
-    GoogleMap,
-    Marker
-} from "react-google-maps";
-import "bootstrap/dist/css/bootstrap.min.css";
+import logo from './logo.svg';
+import './App.css';
 
-const MyMapComponent = withScriptjs(
-    withGoogleMap(props => (
-        <GoogleMap defaultZoom={8} defaultCenter={{lat: 30.7333, lng: 76.7794}}>
-            {props.isMarkerShown && (
-                <Marker position={{lat: 30.7333, lng: 76.7794}}/>
-            )}
-        </GoogleMap>
-    ))
-);
-
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <div className="text-center">
-                    <h1>GOOGLE MAPS API with REACT JS</h1>
-                </div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            MAPS
-                            <div>
-                                <MyMapComponent
-                                    isMarkerShown
-                                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRpNwGR1_Foxo81c_8i4eZzDeeLesmQlY"
-                                    loadingElement={<div style={{height: `100%`}}/>}
-                                    containerElement={<div style={{height: `400px`}}/>}
-                                    mapElement={<div style={{height: `100%`}}/>}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
